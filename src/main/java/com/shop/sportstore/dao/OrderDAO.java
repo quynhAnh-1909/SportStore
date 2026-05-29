9efa4d40e60096f64ecf1e725e1e494239650f56
VoLeQuynhAnh <23130015@st.hcmuaf.edu.vn>
2026-05-29 23:48:10 +0700

    FIX xung đột code

--- src/main/java/com/shop/sportstore/dao/OrderDAO.java
+++ src/main/java/com/shop/sportstore/dao/OrderDAO.java
@@ -761,34 +761,4 @@ public class OrderDAO extends DBConnection {
             e.printStackTrace();
         }
     }
-
-    public Order getOrderByCode(String orderCode) {
-
-        String sql = "SELECT * FROM orders WHERE order_code = ?";
-
-        try (
-                Connection conn = DBConnection.getConnection();
-                PreparedStatement ps = conn.prepareStatement(sql)
-        ) {
-
-            ps.setString(1, orderCode);
-
-            ResultSet rs = ps.executeQuery();
-
-            if (rs.next()) {
-
-                Order order = new Order();
-
-                order.setId(rs.getInt("id"));
-                order.setOrderCode(rs.getString("order_code"));
-
-                return order;
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return null;
-    }
 }
